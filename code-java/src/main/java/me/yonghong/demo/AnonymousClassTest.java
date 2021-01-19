bebcf106bd0af03b5565ae49b1f3b35248566f76
wangyonghong <yonghongwang@163.com>
2021-01-19 20:07:25 +0800

    add AnonymousClassTest

---
+++ code-java/src/main/java/me/yonghong/demo/AnonymousClassTest.java
@@ -0,0 +1,71 @@
+package me.yonghong.demo;
+
+
+/**
+ * @author yonghongwang#163.com
+ */
+public class AnonymousClassTest {
+    public static void main(String[] args) {
+        DoSomething doSomething = new DoSomething();
+        doSomething.run(new OutClassCallback());
+        doSomething.run(new InnerClassCallback());
+        // 匿名内部类
+        doSomething.run(new Callback() {
+            @Override
+            public void doCallback() {
+                fun1();
+                System.out.print("匿名内部类的Callback");
+                printClass();
+            }
+        });
+        // lambda表达式
+        doSomething.run(() -> {
+            fun1();
+            System.out.print("匿名内部类的Callback");
+        });
+    }
+
+    private static void fun1() {
+        System.out.println("调用外层类的方法");
+    }
+
+    static class InnerClassCallback implements Callback {
+        @Override
+        public void doCallback() {
+            System.out.print("内部类的Callback");
+            printClass();
+        }
+    }
+}
+
+interface Callback {
+
+    /**
+     * 执行回调的内容
+     */
+    void doCallback();
+
+    /**
+     * 打印类名
+     */
+    default void printClass() {
+        System.out.println(" ### 类名是：" + getClass());
+        System.out.println();
+    }
+}
+
+class OutClassCallback implements Callback {
+    @Override
+    public void doCallback() {
+        System.out.print("外部类的Callback");
+        printClass();
+    }
+}
+
+class DoSomething {
+    void run(Callback callback) {
+        System.out.println("我要做点啥事");
+        // 做完之后要调用回调
+        callback.doCallback();
+    }
+}
