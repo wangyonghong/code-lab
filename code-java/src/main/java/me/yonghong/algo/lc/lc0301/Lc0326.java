5d10665f4534a2879abec80f3d3c6096b8196f27
wangyonghong <yonghongwang@163.com>
2021-09-23 10:27:59 +0800

    update

---
+++ code-java/src/main/java/me/yonghong/algo/lc/lc0301/Lc0326.java
@@ -0,0 +1,37 @@
+package me.yonghong.algo.lc.lc0301;
+
+import me.yonghong.algo.Solution;
+
+/**
+ * 326. 3的幂
+ * 326. Power of Three
+ *
+ * @author yonghongwang#163.com
+ * @link <a href="https://leetcode-cn.com/problems/power-of-three/"></a>
+ * @link <a href="https://leetcode.com/problems/power-of-three/"></a>
+ * @since 2021/09/23
+ **/
+public class Lc0326 implements Solution {
+
+    public static void main(String[] args) {
+        new Lc0326().test();
+    }
+
+    @Override
+    public void test() {
+        System.out.println(isPowerOfThree(27));
+        System.out.println(isPowerOfThree(9));
+        System.out.println(isPowerOfThree(0));
+        System.out.println(isPowerOfThree(45));
+    }
+
+    public boolean isPowerOfThree(int n) {
+        if (n < 1) {
+            return false;
+        }
+        while (n > 1 && n % 3 == 0) {
+            n = n / 3;
+        }
+        return n == 1;
+    }
+}
