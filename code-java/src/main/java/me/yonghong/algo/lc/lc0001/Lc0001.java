abb3f1bc289075c61c53b31844fafce5586f44ae
wangyonghong <yonghongwang@163.com>
2022-02-23 20:11:50 +0800

    update

--- code-java/src/main/java/me/yonghong/algo/lc/lc0001/Lc0001.java
+++ code-java/src/main/java/me/yonghong/algo/lc/lc0001/Lc0001.java
@@ -8,6 +8,25 @@ import java.util.Map;
 /**
  * 1. 两数之和
  * 1. Two Sum
+ * <p>
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出和为目标值 target 的那两个整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * 你可以按任意顺序返回答案。
+ * <p>
+ * 示例 1：
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * <p>
+ * 示例 2：
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * <p>
+ * 示例 3：
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ * <p>
+ * 进阶：你可以想出一个时间复杂度小于 O(n^2) 的算法吗？
  *
  * @author yonghongwang#163.com
  * @link <a href="https://leetcode-cn.com/problems/two-sum/"></a>
